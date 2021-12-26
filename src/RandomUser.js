5e29b2b074fe2c25f7c9050b495cd184596fa90d
Basar Buyukkahraman <basarbk@yahoo.com>
2021-12-26 17:59:45 +0300

    Add axios and make api call

--- src/RandomUser.js
+++ src/RandomUser.js
@@ -1,3 +1,24 @@
+import axios from 'axios';
+import { useState } from 'react';
+
 export default function RandomUser(){
-  return <button>Load Random User</button>;
+
+  const [user, setUser] = useState();
+
+  const loadRandomUser = async () => {
+    try {
+      const response = await axios.get('https://randomuser.me/api')
+      setUser(response.data.results[0])
+    } catch (error) {
+    }
+  }
+
+  return (
+    <>
+      <button onClick={loadRandomUser}>Load Random User</button>
+      {user && (
+        <h1>{`${user.name.title} ${user.name.first} ${user.name.last}`}</h1>
+      )}
+    </>
+  );
 }
--- src/RandomUser.spec.js
+++ src/RandomUser.spec.js
@@ -1,5 +1,7 @@
 import RandomUser from './RandomUser';
 import { render, screen } from '@testing-library/react';
+import axios from 'axios';
+import userEvent from '@testing-library/user-event';
 
 describe('Random User', () => {
   it('has button to load random user', () => {
@@ -8,5 +10,29 @@ describe('Random User', () => {
       name: 'Load Random User'
     });
     expect(loadButton).toBeInTheDocument();
+  });
+
+  it('displays title, first and lastname of loaded user from randomuser.me', async () => {
+    render(<RandomUser />);
+    const loadButton = screen.queryByRole('button', {
+      name: 'Load Random User'
+    });
+    const mockApiCall = jest.fn().mockResolvedValue({
+      data: {
+        results: [
+          {
+            name: {
+              title: 'Miss',
+              first: 'Jennifer',
+              last: 'Alvarez'
+            }
+          }
+        ]
+      }
+    });
+    axios.get = mockApiCall;
+    userEvent.click(loadButton);
+    const userInfo = await screen.findByText("Miss Jennifer Alvarez");
+    expect(userInfo).toBeInTheDocument();
   });
 });
